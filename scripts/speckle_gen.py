7c77c320dcf1c59a29c618c431f428f4b5a1945b
Michael Atkinson <mike@sprma.com>
2024-06-20 11:01:39 +0100

    Move rounding of position to last possible point

--- scripts/speckle_gen.py
+++ scripts/speckle_gen.py
@@ -6,6 +6,7 @@ feat_size = 10
 spacing = 5
 noise_size = 10
 
+
 # Create feature mask
 i, j = np.meshgrid(np.arange(feat_size), np.arange(feat_size))
 feature = np.sqrt((i-feat_size/2)**2 + (j-feat_size/2)**2) < feat_size/2 -1
@@ -15,16 +16,13 @@ grid_size = tuple(int(x / (feat_size + spacing)) for x in im_size)
 grid_points = np.meshgrid(
     *(np.linspace(spacing, im_size[i]-feat_size-spacing, grid_size[i]) for i in range(2))
 )
-grid_points = np.array(
-    list(zip(*(gp.round().flat for gp in grid_points)))
-)
+grid_points = np.array(list(zip(*(gp.flat for gp in grid_points))))
 grid_points += np.random.uniform(
     -noise_size/2, 
     noise_size/2, 
     grid_size[0]*grid_size[1]*2
 ).reshape(grid_size[0]*grid_size[1], 2)
-grid_points = grid_points.astype(int)
-
+grid_points = grid_points.round().astype(int)
 
 # Insert masks into image at grid points
 image = np.zeros(im_size)
